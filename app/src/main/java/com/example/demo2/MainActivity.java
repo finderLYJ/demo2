ba475c3da01d9efa3f1c87ed3606dfeb062b66cf
李宇进 <3494713938@qq.com>
2023-03-15 16:27:41 +0800

    3月15日下午16:27

--- app/src/main/java/com/example/demo2/MainActivity.java
+++ app/src/main/java/com/example/demo2/MainActivity.java
@@ -55,6 +55,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
             } catch (SecurityException se){
                 se.printStackTrace();
+
+
             }
         }
     }
